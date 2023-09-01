de7e55b4b32b23e757cf043c10901ec17697c350
taparra2 <ataparra20@gmail.com>
2023-09-01 16:19:41 -0500

    added borders for plane, trying to implement bullets

--- myproj/Plane.py
+++ myproj/Plane.py
@@ -1,21 +1,9 @@
 # Abstracat class for olane in the game.
 # Vehicle should have velocity up, velocity down, left, right and other properties that all vehicles share
-from abc import ABC, abstractmethod
 
-
-class Plane(ABC):
+class Plane():
     
-    def __init__(self, name, velocity, bulletVelocity):
+    def __init__(self, name):
         self.name = name
-        self.velocity = velocity
-        self.bulletVelocity = bulletVelocity
-
-    
-    @abstractmethod
-    def movement(self):
-        pass
-    @abstractmethod
-    def bullet(self):
-        pass
-    
+      
 
